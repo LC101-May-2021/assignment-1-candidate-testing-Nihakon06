56dc5b1164a932ab3f6f08fb03be9827f1324b70
nihakon <>
2021-05-24 22:48:53 +0000

    printed greet message with candidateName

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,7 +4,10 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = input.question("Enter Your Name :");
+
+let candidateName;
+candidateName = '';
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
@@ -15,11 +18,9 @@ let candidateAnswers;
 
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
- // Greet the candidate//
-  console.log("Hello, " +candidateName + "!");
+// TODO 1.1b: Ask for candidate's name //
 
-
+candidateName = input.question("Enter Your Name : ");
 
 }
 
@@ -43,7 +44,8 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
- 
+  // Greet the candidate
+  console.log("Hello, " +candidateName +"!");
 
   askQuestion();
   gradeQuiz(this.candidateAnswers);
