ea7d5d51300374b68a786371076fc7b9aedcf90f
nihakon <>
2021-05-24 20:07:01 +0000

    ask candidate for name

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,9 +1,10 @@
 const input = require('readline-sync');
 
+
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
@@ -15,6 +16,9 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+candidateName = input.question("Enter Your Name : ");
+
+
 
 }
 
@@ -36,9 +40,11 @@ function gradeQuiz(candidateAnswers) {
 }
 
 function runProgram() {
-  askForName();
+    askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  // Greet the candidate//
+  console.log("Hello, " +candidateName + "!");
+
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
