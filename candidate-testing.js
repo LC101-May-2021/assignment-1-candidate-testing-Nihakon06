4b91930d6bb41abddf4fb6359cc6ef54bee16734
nihakon <>
2021-06-06 20:00:37 +0000

    removed return from askForName

--- candidate-testing.js
+++ candidate-testing.js
@@ -23,7 +23,7 @@ let candidateAnswers = [];
 function askForName() {
 // TODO 1.1b: Ask for candidate's name //
 candidateName = input.question("Candidate Name: ");
-return candidateName;
+
 }
 
 // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
@@ -92,30 +92,3 @@ module.exports = {
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
-
-
-/*
-Candidate Name: Can Twin
-1) Who was the first American woman in space?
-Your Answer: sally ride
-Correct Answer: Sally Ride
-
-2) True or false: 5000 meters = 5 kilometers.
-Your Answer: false
-Correct Answer: true
-
-3) (5 + 3)/2 * 10 = ?
-Your Answer: 45
-Correct Answer: 40
-
-4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?
-Your Answer: trajectory
-Correct Answer: Trajectory
-
-5) What is the minimum crew size for the ISS?
-Your Answer: 10
-Correct Answer: 3
-
->>> Overall Grade: 40% (2 of 5 responses correct) <<<
->>> Status: FAILED <<<
-*/
