46fdd17fe7e9773478971c735b86ef0dcd36040a
nihakon <>
2021-06-06 16:50:20 +0000

    error at 49

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,60 +1,82 @@
+// readline-sync is used have an interactive code that is used to have conversation with the user.
 const input = require('readline-sync');
-
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-
+// Initialized a variable called candidateName.
 let candidateName;
-candidateName = '';
+// candidateName is assigned with an empty string.
+candidateName = "";
 
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer 
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+
+// Define questions and correctAnswers variables as arrays. Use the table below to fill these arrays.
+let questions = ['Who was the first American woman in space?', 'True or false: 5000 meters = 5 kilometers.', '(5 + 3)/2 * 10 = ?', 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?', 'What is the minimum crew size for the ISS?'];
+let correctAnswers = ['Sally Ride', 'true', '40','Trajectory','3'];
+let candidateAnswers = [];
 
 
 function askForName() {
 // TODO 1.1b: Ask for candidate's name //
-
-candidateName = input.question("Enter Your Name : ");
-
+candidateName = input.question("Enter Your Name: ");
+return candidateName;
 }
 
-function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-candidateAnswer = input.question(question);
-console.log(candidateAnswer);
+// TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+// TODO 2: modify your quiz app to ask 5 questions
+// Define questions and correctAnswers variables as arrays. Use the table below to fill these arrays.
+function askQuestions() {
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswer = input.question(questions[i]);
+    candidateAnswers.push(String(candidateAnswer));
+  }
 }
 
+
+// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+// if (candidateAnswer === correctAnswer){
+//  console.log("Your Answer is Correct!")
+// } else {
+//   console.log("Your Answer is Wrong!")
+// }
 function gradeQuiz(candidateAnswers) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer){
-  console.log("Your Answer is Correct!")
-} else {
-  console.log("Your Answer is Wrong!")
-}
-
-  let grade;
-  
-
+  let noOfCorrectAns = 0;
+  let noOfQuizQues = questions.length;
+  for (let i = 0; i < candidateAnswers.length; i++) {
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+    noOfCorrectAns =+ 1;
+    }
+  }
+  let grade = ((noOfCorrectAns / noOfQuizQues) * 100);
+  let status = '';
+  if (grade >= 80) {
+    status = "PASSED ";
+  } else {
+    status = "FAILED";
+    }
   return grade;
 }
+
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   // Greet the candidate
-  console.log("Hello, " +candidateName +"!");
-
-  askQuestion();
+  // Replace the basic feedback from TODO 1.2c with a template literal that displays each of the candidate's responses in addition to the corresponding correct answers.
+  askQuestions();
   gradeQuiz(this.candidateAnswers);
+  console.log(`Candidate Name: ${candidateName} \n ${i}) ${questions[i]} \nYour Answer: ${candidateAnswers[i]} \n Correct Answer: ${correctAnswers[i]}`);
+  console.log(`>>> Overall Grade: ${grade} (${noOfCorrectAns} of ${noOfQuizQues} responses correct) <<<
+  >>> Status: ${status} <<<`);
+  
 }
+
+runProgram();
+ 
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
